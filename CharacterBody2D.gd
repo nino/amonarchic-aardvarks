8f6b8092c6d6d942110f04cbc320f404098691b6
Nino Annighoefer <nino@ninoan.com>
2023-05-08 14:02:55 +0100

    More opaque please

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -18,6 +18,11 @@ func _physics_process(delta):
 	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 	# 	velocity.y = JUMP_VELOCITY
 
+	# Flip the sprite based on the direction.
+	if velocity.x != 0:
+		$PuffinSweaty.flip_h = velocity.x >= 0
+
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var x_direction = Input.get_axis("ui_left", "ui_right")
