9a0a0ff52726530fb08710a36f2b439bd8fa4c44
Nino Annighoefer <nino@ninoan.com>
2023-05-08 16:10:05 +0100

    Chestbump handoff almost works!!!

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,57 +1,72 @@
 extends CharacterBody2D
 
+signal bumped
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_active = false
 
 var chestbump_is_pressed = false
 
 func _walking_animation():
 	# Flip the sprite based on the direction.
 	if velocity.x != 0:
-		$PuffinSweaty.flip_h = velocity.x >= 0
+		$Pic.flip_h = velocity.x >= 0
 
 	var time = Time.get_ticks_msec() / 1000.0
 	var angle_offset = sin(time * 24.0) * 0.4
 	
 	# If he is walking, alternate between rotating 20degrees and -20degrees.
 	if velocity.x != 0 || velocity.y != 0:
-		$PuffinSweaty.rotation = lerp_angle($PuffinSweaty.rotation, 0.1 * sign(velocity.x) + angle_offset, 0.1)
+		$Pic.rotation = lerp_angle($Pic.rotation, 0.1 * sign(velocity.x) + angle_offset, 0.1)
 	else:
-		$PuffinSweaty.rotation = lerp_angle($PuffinSweaty.rotation, 0, 0.1)
+		$Pic.rotation = lerp_angle($Pic.rotation, 0, 0.1)
+
+# handle `bumped` signal
+func _on_bumped():
+	print("Singal received")
+	is_active = !is_active
+
+func perform_chestbump():
+	var animation = "chestbump_animation_left"
+	if $Pic.flip_h:
+		animation = "chestbump_animation"
+	
+	$Pic/PuffinAnimations.seek(0)
+	$Pic/PuffinAnimations.play(animation)
+
+	emit_signal("bumped")
+
 
 func _chestbump():
-	var animation = "chestbump_animation_left"
-	if $PuffinSweaty.flip_h:
-		animation = "chestbump_animation"
 	if Input.is_key_pressed(KEY_Q):
 		if chestbump_is_pressed == false:
-			$PuffinSweaty/PuffinAnimations.seek(0)
-			$PuffinSweaty/PuffinAnimations.play(animation)
+			perform_chestbump()
 		chestbump_is_pressed = true
 	else:
 		chestbump_is_pressed = false
 
 
 func _physics_process(delta):
-	_walking_animation()
-	_chestbump()
+	if is_active:
+		_walking_animation()
+		_chestbump()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var x_direction = Input.get_axis("ui_left", "ui_right")
-	var y_direction = Input.get_axis("ui_up", "ui_down")
-	if y_direction:
-		velocity.y = y_direction * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-		
-	if x_direction:
-		velocity.x = x_direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var x_direction = Input.get_axis("ui_left", "ui_right")
+		var y_direction = Input.get_axis("ui_up", "ui_down")
+		if y_direction:
+			velocity.y = y_direction * SPEED
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+			
+		if x_direction:
+			velocity.x = x_direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
---
+++ Puffin.gd
@@ -0,0 +1,12 @@
+extends "res://CharacterBody2D.gd"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	is_active = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# is_active = true
+	pass
+
